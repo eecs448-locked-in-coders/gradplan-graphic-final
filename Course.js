406d555e0d17464efbb6a2c50fe356351a4edb86
Rafael Alaras <codenameb4lyfe@gmail.com>
2020-10-10 17:09:06 -0500

    Add constructors

--- Course.js
+++ Course.js
@@ -1,2 +1,18 @@
+//Has a course code, prerequisite, corequisites, course offering, credit hours
 class Course {
+  /*
+    course_code = string
+    prereq = [string]
+    coreq = [string]
+    course_offer = [bool,bool,bool]
+    credit_hour = int
+  */
+  constructor(course_code, prereq, coreq, course_offer, credit_hour){
+    this.course_code = course_code;
+    this.prereq = prereq;
+    this.coreq = coreq;
+    this.course_offer = course_offer;
+    this.credit_hour = credit_hour;
+  }
+
 }
